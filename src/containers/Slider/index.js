72cf726179345635f891bbf9c16f8b8943dcc75d
Ebru Dincer <ebru.dincer16@gmail.com>
2024-02-27 18:15:06 +0100

    Correction key du slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -25,9 +25,9 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div
+        key={event.title}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -45,7 +45,7 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${crypto.randomUUID()}`}
                   type="radio"
                   name="radio-button"
                   defaultChecked={index === radioIdx}
@@ -53,7 +53,7 @@ const Slider = () => {
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
